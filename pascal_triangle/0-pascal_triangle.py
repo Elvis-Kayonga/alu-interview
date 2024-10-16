02dfb170adb9da1f14955de36f16930be5e90970
KAYONGA Elvis <126350069+Elvis-Kayonga@users.noreply.github.com>
2024-10-16 19:50:11 +0300

    Update 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,23 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle module.
+This module provides a function to generate Pascal's triangle.
+"""
+
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n.
+    Returns an empty list if n <= 0.
+    
+    Args:
+        n (int): The number of rows of the triangle to generate.
+
+    Returns:
+        List[List[int]]: A list of lists, where each sublist represents a row in Pascal's triangle.
+    """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]  # First row is always [1]
     
     for i in range(1, n):
